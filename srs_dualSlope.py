c2d82141179e663f3dd4d34acfd9461b132dcef8
Darshana Gopal <155978350+rmapdgo@users.noreply.github.com>
2025-05-19 09:33:38 +0100

    Update srs_dualSlope.py

--- srs_dualSlope.py
+++ srs_dualSlope.py
@@ -1,198 +1,102 @@
+
+
 import numpy as np
 import pandas as pd
-#==================================SRS======================================887=============================================
 
+def SRS(data: pd.DataFrame):
+    wavelengths = [782, 801, 808, 828, 848, 887]
+    extinction_path = "src/concentrations_ucln_srs/defaults.csv"
+    epsilon = pd.read_csv(extinction_path)
 
-def SRS(atten_a_1, atten_a_2, atten_a_3, atten_b_1, atten_b_2, atten_b_3):
-    
-    print('atten_a_1', atten_a_1)
-    print('atten_a_2', atten_a_2)
-    print('atten_a_3', atten_a_3)
+    n_samples = len(data)
 
-    print('atten_b_1', atten_b_1)
-    print('atten_b_2', atten_b_2)
-    print('atten_b_3', atten_b_3)
-    wavelengths = np.array([782, 801, 808, 828, 848, 887 ])
+    # Get attenuation data (shape: 6 wavelengths × samples × 3 detectors)
+    def get_group_matrix(prefix):
+        group_matrix = np.empty((6, n_samples, 3))
+        for i, wl in enumerate(wavelengths):
+            for d in range(1, 4):
+                col = f'{prefix}_{wl}_DET{d}'
+                group_matrix[i, :, d-1] = pd.to_numeric(data[col], errors='coerce').fillna(1e-10).values
+        return group_matrix
 
-    srs_extinction_coefficients = pd.read_csv(
-    "src/concentrations_ucln_srs/defaults.csv")
+    intensity_A = get_group_matrix("LED_A")
+    intensity_B = get_group_matrix("LED_B")
 
-    srs_ex_co = []
-    for wavelength in wavelengths:
-        for i, j in enumerate(srs_extinction_coefficients.iloc[:, 0]):
-            if wavelength == j:
-                srs_ex_co.append(srs_extinction_coefficients.iloc[i, :])
+    # Compute attenuation: log10(ref / intensity), ref=100
+    attenuation_A = np.log10(100 / np.clip(intensity_A, 1e-10, None))
+    attenuation_B = np.log10(100 / np.clip(intensity_B, 1e-10, None))
 
-    srs_ex_co = np.array(srs_ex_co)
-    srs_ext_coeffs = srs_ex_co[:, 1:3]  # just take oxy and deoxy for now
-    print('ext_coeffs', srs_ext_coeffs)
-    #ext_coeffs_t = ext_coeffs.T
-    srs_ext_coeffs_inv = np.linalg.pinv(srs_ext_coeffs)
-    print('ext_coeffs', srs_ext_coeffs_inv)
-    atten_a_1.columns = ['LED_A_782_DET1', 'LED_A_801_DET1', 'LED_A_808_DET1', 'LED_A_828_DET1', 'LED_A_848_DET1',
-                     'LED_A_887_DET1']
-    atten_a_2.columns = ['LED_A_782_DET2', 'LED_A_801_DET2', 'LED_A_808_DET2', 'LED_A_828_DET2', 'LED_A_848_DET2',
-                     'LED_A_887_DET2']
-    atten_a_3.columns = ['LED_A_782_DET3', 'LED_A_801_DET3', 'LED_A_808_DET3', 'LED_A_828_DET3', 'LED_A_848_DET3',
-                     'LED_A_887_DET3']
+    # Optode distances in cm
+    optode_distance_A = np.array([3, 4, 5]).reshape(-1, 1)
+    optode_distance_B = np.array([5, 4, 3]).reshape(-1, 1)
+    dets_a = [0, 1, 2]
+    dets_b = [0, 1, 2]
 
-    atten_b_1.columns = ['LED_B_782_DET1', 'LED_B_801_DET1', 'LED_B_808_DET1', 'LED_B_828_DET1', 'LED_B_848_DET1',
-                     'LED_B_887_DET1']
-    atten_b_2.columns = ['LED_B_782_DET2', 'LED_B_801_DET2', 'LED_B_808_DET2', 'LED_B_828_DET2', 'LED_B_848_DET2',
-                     'LED_B_887_DET2']
-    atten_b_3.columns = ['LED_B_782_DET3', 'LED_B_801_DET3', 'LED_B_808_DET3', 'LED_B_828_DET3', 'LED_B_848_DET3',
-                     'LED_B_887_DET3']
+    # Select attenuation for each detector
+    atten_a = attenuation_A[:, :, dets_a]  # shape: (6, samples, 3)
+    atten_b = attenuation_B[:, :, dets_b]
 
-    Group_A_782 = [atten_a_1['LED_A_782_DET1'], atten_a_2['LED_A_782_DET2'], atten_a_3['LED_A_782_DET3']]
-    Group_A_801 = [atten_a_1['LED_A_801_DET1'], atten_a_2['LED_A_801_DET2'], atten_a_3['LED_A_801_DET3']]
-    Group_A_808 = [atten_a_1['LED_A_808_DET1'], atten_a_2['LED_A_808_DET2'], atten_a_3['LED_A_808_DET3']]
-    Group_A_828 = [atten_a_1['LED_A_828_DET1'], atten_a_2['LED_A_828_DET2'], atten_a_3['LED_A_828_DET3']]
-    Group_A_848 = [atten_a_1['LED_A_848_DET1'], atten_a_2['LED_A_848_DET2'], atten_a_3['LED_A_848_DET3']]
-    Group_A_887 = [atten_a_1['LED_A_887_DET1'], atten_a_2['LED_A_887_DET2'], atten_a_3['LED_A_887_DET3']]
+    # Slopes (m) for each wavelength and sample
+    m_A_og = np.empty((6, n_samples))
+    m_B_og = np.empty((6, n_samples))
 
-    Group_B_782 = [atten_b_1['LED_B_782_DET1'], atten_b_2['LED_B_782_DET2'], atten_b_3['LED_B_782_DET3']]
-    Group_B_801 = [atten_b_1['LED_B_801_DET1'], atten_b_2['LED_B_801_DET2'], atten_b_3['LED_B_801_DET3']]
-    Group_B_808 = [atten_b_1['LED_B_808_DET1'], atten_b_2['LED_B_808_DET2'], atten_b_3['LED_B_808_DET3']]
-    Group_B_828 = [atten_b_1['LED_B_828_DET1'], atten_b_2['LED_B_828_DET2'], atten_b_3['LED_B_828_DET3']]
-    Group_B_848 = [atten_b_1['LED_B_848_DET1'], atten_b_2['LED_B_848_DET2'], atten_b_3['LED_B_848_DET3']]
-    Group_B_887 = [atten_b_1['LED_B_887_DET1'], atten_b_2['LED_B_887_DET2'], atten_b_3['LED_B_887_DET3']]
+    def get_slope(att, distances):
+        att = att.flatten()
+        distances = distances.flatten()
+        A = np.vstack([distances, np.ones_like(distances)]).T
+        return np.linalg.lstsq(A, att, rcond=None)[0][0]
 
-    detector_A_distance = np.array([[3], [4], [5]])
-    detector_B_distance = np.array([[5], [4], [3]])
+    for l in range(6):
+        for t in range(n_samples):
+            m_A_og[l, t] = get_slope(atten_a[l, t, :], optode_distance_A)
+            m_B_og[l, t] = get_slope(atten_b[l, t, :], optode_distance_B)
 
-    ##Do group A first##
-    A_groupA = np.hstack((detector_A_distance,np.ones_like(detector_A_distance) ))
-
-    # 782nm
-    atten_wldep_A_782 = np.array(Group_A_782, dtype=float)  # change atten_wldp with group from above
-    m_782_A = np.zeros((1, len(atten_wldep_A_782[0])))
-    c_782_A = np.zeros((1, len(atten_wldep_A_782[0])))
-    for x in range(0, len(atten_wldep_A_782[0])):
-        m_782_A[0, x], c_782_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_782[:, x], rcond=None)[0]
-
-    # 801nm
-    atten_wldep_A_801 = np.array(Group_A_801, dtype=float)  # change atten_wldp with group from above
-    m_801_A = np.zeros((1, len(atten_wldep_A_801[0])))
-    c_801_A = np.zeros((1, len(atten_wldep_A_801[0])))
-    for x in range(0, len(atten_wldep_A_801[0])):
-        m_801_A[0, x], c_801_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_801[:, x], rcond=None)[0]
-
-    # 808nm
-    atten_wldep_A_808 = np.array(Group_A_808, dtype=float)  # change atten_wldp with group from above
-    m_808_A = np.zeros((1, len(atten_wldep_A_808[0])))
-    c_808_A = np.zeros((1, len(atten_wldep_A_808[0])))
-    for x in range(0, len(atten_wldep_A_808[0])):
-        m_808_A[0, x], c_808_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_808[:, x], rcond=None)[0]
-
-    # 828nm
-    atten_wldep_A_828 = np.array(Group_A_828, dtype=float)  # change atten_wldp with group from above
-    m_828_A = np.zeros((1, len(atten_wldep_A_828[0])))
-    c_828_A = np.zeros((1, len(atten_wldep_A_828[0])))
-    for x in range(0, len(atten_wldep_A_808[0])):
-        m_828_A[0, x], c_828_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_828[:, x], rcond=None)[0]
-
-    # 848nm
-    atten_wldep_A_848 = np.array(Group_A_848, dtype=float)  # change atten_wldp with group from above
-    m_848_A = np.zeros((1, len(atten_wldep_A_848[0])))
-    c_848_A = np.zeros((1, len(atten_wldep_A_848[0])))
-    for x in range(0, len(atten_wldep_A_848[0])):
-        m_848_A[0, x], c_848_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_848[:, x], rcond=None)[0]
-
-
-    # 887nm
-    atten_wldep_A_887 = np.array(Group_A_887, dtype=float)  # change atten_wldp with group from above
-    m_887_A = np.zeros((1, len(atten_wldep_A_887[0])))
-    c_887_A = np.zeros((1, len(atten_wldep_A_887[0])))
-    for x in range(0, len(atten_wldep_A_887[0])):
-        m_887_A[0, x], c_887_A[0, x] = np.linalg.lstsq(A_groupA, atten_wldep_A_887[:, x], rcond=None)[0]
-
-    ##Do group B##
-    A_groupB = np.hstack((detector_B_distance,np.ones_like(detector_B_distance) ))
-
-    # 782nm
-    atten_wldep_B_782 = np.array(Group_B_782, dtype=float)  # change atten_wldp with group from above
-    m_782_B = np.zeros((1, len(atten_wldep_B_782[0])))
-    c_782_B = np.zeros((1, len(atten_wldep_B_782[0])))
-    for x in range(0, len(atten_wldep_B_782[0])):
-        m_782_B[0, x], c_782_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_782[:, x], rcond=None)[0]
-
-    # 801nm
-    atten_wldep_B_801 = np.array(Group_B_801, dtype=float)  # change atten_wldp with group from above
-    m_801_B = np.zeros((1, len(atten_wldep_B_801[0])))
-    c_801_B = np.zeros((1, len(atten_wldep_B_801[0])))
-    for x in range(0, len(atten_wldep_B_801[0])):
-        m_801_B[0, x], c_801_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_801[:, x], rcond=None)[0]
-
-    # 808nm
-    atten_wldep_B_808 = np.array(Group_B_808, dtype=float)  # change atten_wldp with group from above
-    m_808_B = np.zeros((1, len(atten_wldep_B_808[0])))
-    c_808_B = np.zeros((1, len(atten_wldep_B_808[0])))
-    for x in range(0, len(atten_wldep_B_808[0])):
-        m_808_B[0, x], c_808_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_808[:, x], rcond=None)[0]
-
-    # 828nm
-    atten_wldep_B_828 = np.array(Group_B_828, dtype=float)  # change atten_wldp with group from above
-    m_828_B = np.zeros((1, len(atten_wldep_B_828[0])))
-    c_828_B = np.zeros((1, len(atten_wldep_B_828[0])))
-    for x in range(0, len(atten_wldep_B_828[0])):
-        m_828_B[0, x], c_828_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_828[:, x], rcond=None)[0]
-
-
-    # 848nm
-    atten_wldep_B_848 = np.array(Group_B_848, dtype=float)  # change atten_wldp with group from above
-    m_848_B = np.zeros((1, len(atten_wldep_B_848[0])))
-    c_848_B = np.zeros((1, len(atten_wldep_B_848[0])))
-    for x in range(0, len(atten_wldep_B_848[0])):
-        m_848_B[0, x], c_848_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_848[:, x], rcond=None)[0]
-
-    # 887nm
-    atten_wldep_B_887 = np.array(Group_B_887, dtype=float)  # change atten_wldp with group from above
-    m_887_B = np.zeros((1, len(atten_wldep_B_887[0])))
-    c_887_B = np.zeros((1, len(atten_wldep_B_887[0])))
-    for x in range(0, len(atten_wldep_B_887[0])):
-        m_887_B[0, x], c_887_B[0, x] = np.linalg.lstsq(A_groupB, atten_wldep_B_887[:, x], rcond=None)[0]
-
+    # Compute k_mua
     h = 6.3e-4
-    # group A
-    k_mua_782_A = (1 / (3 * (1 - (h * wavelengths[0])))) * (np.log(10) * m_782_A - (2 / np.mean(detector_A_distance)))**2
-    k_mua_801_A = (1 / (3 * (1 - (h * wavelengths[1])))) * (np.log(10) * m_801_A - (2 / np.mean(detector_A_distance)))**2
-    k_mua_808_A = (1 / (3 * (1 - (h * wavelengths[2])))) * (np.log(10) * m_808_A - (2 / np.mean(detector_A_distance)))**2
-    k_mua_828_A = (1 / (3 * (1 - (h * wavelengths[3])))) * (np.log(10) * m_828_A - (2 / np.mean(detector_A_distance)))**2
-    k_mua_848_A = (1 / (3 * (1 - (h * wavelengths[4])))) * (np.log(10) * m_848_A - (2 / np.mean(detector_A_distance)))**2
-    k_mua_887_A = (1 / (3 * (1 - (h * wavelengths[5])))) * (np.log(10) * m_887_A - (2 / np.mean(detector_A_distance)))**2
+    k_mua_A = np.empty((6, n_samples))
+    k_mua_B = np.empty((6, n_samples))
 
-    #groupB
-    k_mua_782_B = (1 / (3 * (1 - (h * wavelengths[0])))) * (np.log(10) * m_782_B - (2 / np.mean(detector_B_distance)))**2
-    k_mua_801_B = (1 / (3 * (1 - (h * wavelengths[1])))) * (np.log(10) * m_801_B - (2 / np.mean(detector_B_distance)))**2
-    k_mua_808_B = (1 / (3 * (1 - (h * wavelengths[2])))) * (np.log(10) * m_808_B - (2 / np.mean(detector_B_distance)))**2
-    k_mua_828_B = (1 / (3 * (1 - (h * wavelengths[3])))) * (np.log(10) * m_828_B - (2 / np.mean(detector_B_distance)))**2
-    k_mua_848_B = (1 / (3 * (1 - (h * wavelengths[4])))) * (np.log(10) * m_848_B - (2 / np.mean(detector_B_distance)))**2
-    k_mua_887_B = (1 / (3 * (1 - (h * wavelengths[5])))) * (np.log(10) * m_887_B - (2 / np.mean(detector_B_distance)))**2
+    for l in range(6):
+        h_corr = 1 - h * wavelengths[l]
+        denominator = 3 * h_corr
+        mean_A = np.mean(optode_distance_A)
+        mean_B = np.mean(optode_distance_B)
 
+        for t in range(n_samples):
+            term_A = np.log(10) * m_A_og[l, t] - (2 / mean_A)
+            term_B = np.log(10) * m_B_og[l, t] - (2 / mean_B)
 
-    k_mua_A = np.vstack([k_mua_782_A, k_mua_801_A,k_mua_808_A,k_mua_828_A,k_mua_848_A, k_mua_887_A])
-    k_mua_B = np.vstack([k_mua_782_B, k_mua_801_B,k_mua_808_B,k_mua_828_B,k_mua_848_B, k_mua_887_B])
+            k_mua_A[l, t] = (1 / denominator) * (term_A ** 2)
+            k_mua_B[l, t] = (1 / denominator) * (term_B ** 2)
 
-    C_A = np.matmul(srs_ext_coeffs_inv, k_mua_A)
-    C_B = np.matmul(srs_ext_coeffs_inv, k_mua_B)
+    # Get extinction coefficients (HbO2, HHb) only
+    ext_coeffs_srs = []
+    for wl in wavelengths:
+        row = epsilon[epsilon['wavelength'] == wl]
+        if not row.empty:
+            ext = row[['HbO2', 'HHb']].values.flatten()
+            ext_coeffs_srs.append(ext)
 
-    oxy_a = C_A[0]
-    deoxy_a = C_A[1]
+    ext_coeffs_srs = np.array(ext_coeffs_srs)  # (6, 2)
+    ext_inv = np.linalg.pinv(ext_coeffs_srs) * 10 / np.log(10)  # (2, 6)
 
-    oxy_b = C_B[0]
-    deoxy_b = C_B[1]
+    # Calculate concentrations
+    kConc_A = ext_inv @ k_mua_A  # (2, n_samples)
+    kConc_B = ext_inv @ k_mua_B
 
-    sto2_a = (oxy_a / (oxy_a + deoxy_a)) * 100
-    sto2_b = (oxy_b / (oxy_b + deoxy_b)) * 100
+    # Extract HHb and HbO
+    HHb_A, HbO_A = kConc_A[0, :], kConc_A[1, :]
+    HHb_B, HbO_B = kConc_B[0, :], kConc_B[1, :]
 
-    print( "k_mua_A", k_mua_A)
-    print( "k_mua_B", k_mua_B)
-    print( "C_A", C_A)
-    print( "C_B", C_B)
-    print( "oxy_a", oxy_a)
-    print( "deoxy_a", deoxy_a)
-    print( "oxy_a", oxy_b)
-    print( "deoxy_a", deoxy_b)
+    # Total hemoglobin
+    HbT_A = HbO_A + HHb_A
+    HbT_B = HbO_B + HHb_B
 
-    return sto2_a, sto2_b, srs_ext_coeffs_inv
+    # Calculate StO2
+    sto2_A = (HbO_A / np.clip(HbT_A, 1e-10, None)) * 100
+    sto2_B = (HbO_B / np.clip(HbT_B, 1e-10, None)) * 100
 
+    return {
+        'StO2_A': sto2_A,
+        'StO2_B': sto2_B
+    }
